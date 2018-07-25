d6fe3b6bd07dbfff3bb5db22865d7dfd2f5f5fa0
Gershon <reydmang@myumanitoba.ca>
2018-07-25 14:56:43 -0500

    Conributers JavaScript

--- buzzword.js
+++ buzzword.js
@@ -19,3 +19,22 @@ function processPull()  {
   }
 }
 
+function getContributers() {
+	xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = processContributers;
+	xhr.open("POST", "cgi-bin/get-contributers.cgi");
+	xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded; charset=UTF-8')
+	xhr.send();
+}
+
+function processContributers()  {
+  // only if req shows "loaded"
+  if (xhr.readyState == 4) {
+
+    if (xhr.status == 200 || xhr.status == 404 || xhr.status == 500) {
+      var response = xhr.responseText;
+	  console.log(response)
+    }
+  }
+}
+
