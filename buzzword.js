2aaf59f783ce11cda7bc3e535a17e8d26c32b10b
Eric Kulchycki <erickulchycki@gmail.com>
2018-08-01 22:17:01 -0500

    Removed duplicate code

--- buzzword.js
+++ buzzword.js
@@ -1,28 +1,28 @@
 var xhr = "";
 function pullMaster() {
 	xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = processPull;
+	xhr.onreadystatechange = processRequest;
 	xhr.open("POST", "cgi-bin/pull-master.cgi");
 	xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded; charset=UTF-8')
 	xhr.send('method=pull');
 }
 
 
-function processPull()  {
-  // only if req shows "loaded"
-  if (xhr.readyState == 4) {
+// function processPull()  {
+//   // only if req shows "loaded"
+//   if (xhr.readyState == 4) {
 
-    if (xhr.status == 200 || xhr.status == 404 || xhr.status == 500) {
-      var response = xhr.responseText;
-	  console.log(response)
-    }
-  }
-}
+//     if (xhr.status == 200 || xhr.status == 404 || xhr.status == 500) {
+//       var response = xhr.responseText;
+// 	  console.log(response)
+//     }
+//   }
+// }
 
 //Can we change this to jQuery?
 function getContributers() {
 	xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = processContributers;
+	xhr.onreadystatechange = processRequest;
 	xhr.open("POST", "cgi-bin/get-contributers.cgi");
 	xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded; charset=UTF-8')
 	xhr.send(null);
@@ -30,13 +30,24 @@ function getContributers() {
 
 function genWebsite() {
   xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = processContributers;
+  xhr.onreadystatechange = processRequest;
 	xhr.open("POST", "cgi-bin/gen-website.cgi");
 	xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded; charset=UTF-8')
 	xhr.send(null);
 }
 
-function processContributers()  {
+// function processContributers()  {
+//   // only if req shows "loaded"
+//   if (xhr.readyState == 4) {
+
+//     if (xhr.status == 200 || xhr.status == 404 || xhr.status == 500) {
+//       var response = xhr.responseText;
+// 	  console.log(response)
+//     }
+//   }
+// }
+
+function processRequest()  {
   // only if req shows "loaded"
   if (xhr.readyState == 4) {
 
